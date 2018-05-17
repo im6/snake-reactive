056b7441782457bd8eb3df4ae58c60b738005e96
im6 <ee2win@gmail.com>
2018-05-17 11:30:31 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: ".src/app.ts",
+  output: {
+    filename: "bundle.js"
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader"
+      }
+    ]
+  }
+};
