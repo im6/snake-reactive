67a77f154c4d7c5714483ee863255a7c904d226b
im6 <ee2win@gmail.com>
2018-05-27 21:30:26 +0800

    fix lint

--- src/app.ts
+++ src/app.ts
@@ -11,26 +11,26 @@ const canvas = createCanvasElem();
 document.body.appendChild(canvas);
 const ctx = canvas.getContext('2d');
 
-let game$ = Observable.interval(1000, animationFrame)
+const game$ = Observable.interval(1000, animationFrame)
   .subscribe({
     next: (ind) => {
       renderScene(ctx);
       renderApples(ctx, [
-        {x: 1, y: 1},
-        {x: 3, y: 3},
+        { x: 1, y: 1 },
+        { x: 3, y: 3 },
       ]);
       renderSnake(ctx, [
-        {x: 4, y: 4},
-        {x: 4, y: 5},
-        {x: 4, y: 6},
-        {x: 4, y: 7},
+        { x: 4, y: 4 },
+        { x: 4, y: 5 },
+        { x: 4, y: 6 },
+        { x: 4, y: 7 },
       ])
     },
     complete: () => console.log('game over.'),
   });
 
 
-let keydownSource = Observable.fromEvent(document, 'keydown')
+const keydownSource = Observable.fromEvent(document, 'keydown')
   .map((event: KeyboardEvent) => DIRECTIONS[event.keyCode])
   .filter(drc => !!drc)
   .scan(nextDirection)
@@ -41,14 +41,14 @@ const subscribe = keydownSource.subscribe(val => {
   console.log(val);
 });
 
-let length$ = new BehaviorSubject<number>(SNAKE_LENGTH);
+const length$ = new BehaviorSubject<number>(SNAKE_LENGTH);
 
-let snakeLength$ = length$.pipe(
+const snakeLength$ = length$.pipe(
   scan((step, snakeLength) => snakeLength + step),
   share()
 );
 
-let score$ = snakeLength$.pipe(
+const score$ = snakeLength$.pipe(
   startWith(0),
   scan((score, _) => score + POINTS_PER_APPLE),
 );
